0c9b6d8cd4a485332939a7c0831b18314ebd208c
Radosvet Petrov <radosvet.petrov@nutmeg.com>
2020-07-30 16:10:30 +0300

    commit again

---
+++ webpack-federation-storybook-design-systems/App/src/content.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { ShapesIcon } from "xolvio_ui/icons/ShapesIcon";
+import { ScreenIcon } from "xolvio_ui/icons/ScreenIcon";
+import { FlipchartIcon } from "xolvio_ui/icons/FlipchartIcon";
+
+export const content = {
+  title: "Let us up your game",
+  subheading: "SERVICES",
+  sections: [
+    {
+      heading: "Modernization",
+      description: "We can help you with your software delivery strategy to sustainably increase the " + "productivity of your entire team and ensure you are continuously shipping valuable " + "software to your customers.",
+      icon: <ShapesIcon />,
+    },
+    {
+      heading: "Bespoke development",
+      description: "We help to transform systems, modernize solutions and renovate development ecosystems.",
+      icon: <ScreenIcon />,
+    },
+    {
+      heading: "Training",
+      description: "Prepare your team and product for success. Team or 1-1 tuition on all aspects of test " + "automation.",
+      icon: <FlipchartIcon />,
+    },
+  ],
+};
---
+++ webpack-federation-storybook-design-systems/App/webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { StorybookWebpackFederationPlugin } = require("storybook-webpack-federation-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    chunkFilename: "[id].[chunkhash].js",
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [require.resolve("@babel/preset-react")],
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new StorybookWebpackFederationPlugin({
+      remotes: ["xolvio_ui"],
+    }),
+  ],
+};
